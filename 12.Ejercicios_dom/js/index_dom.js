1e821ddbf2788efccdeb6c041c2a1643f7678c2b
ileanachamorro <ic.chamorro07@gmail.com>
2022-11-03 14:45:07 -0300

    DOM: Ejercicios Practicos | Eventos del Teclado (Atajos

--- 12.Ejercicios_dom/js/index_dom.js
+++ 12.Ejercicios_dom/js/index_dom.js
@@ -1,5 +1,6 @@
 import hamburgerMenu from "../dom/script.js";
 import { digitalClock, alarm } from "../dom/reloj.js";
+import { shortcuts } from "../dom/teclado.js";
 const d = document;
 
 d.addEventListener("DOMContentLoaded", (e) => {
@@ -7,3 +8,7 @@ d.addEventListener("DOMContentLoaded", (e) => {
     digitalClock("#reloj", "#activar-reloj", "#desactivar-reloj");
     alarm("assets/car-alarm.mp3", "#activar-alarma", "#desactivar-alarma");
 });
+
+d.addEventListener("keydown", (e) => {
+    shortcuts(e);
+})
