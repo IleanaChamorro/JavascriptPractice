83587e60f55fe327601c8f32f0a423ed028c5994
ileanachamorro <ic.chamorro07@gmail.com>
2022-11-08 15:55:35 -0300

    DOM: Ejercicios | Eventos del Teclado (movimientos y colisiones)

--- 12.Ejercicios_dom/dom/teclado.js
+++ 12.Ejercicios_dom/dom/teclado.js
@@ -1,25 +1,49 @@
 const d = document;
+let x = 0,
+    y = 0;
 
 export function moveBall(e, ball, stage){
     const $ball = d.querySelector(ball),
-    $stage = d.querySelector(stage);
-    console.log(e.keyCode);
+    $stage = d.querySelector(stage),
+    limitsBall = $ball.getBoundingClientRect(),
+    limitsStage = $stage.getBoundingClientRect()
+    ;
 
+    //console.log(e.keyCode);
+    //console.log(e.key);
+    //console.log(limitsBall, limitsStage);
 
     switch(e.keyCode){
         //izq
         case 37:
+            if(limitsBall.left > limitsStage.left) {
+                e.preventDefault();
+                x--
+            };
             break;
         case 38:
+            if(limitsBall.top > limitsStage.top) {
+                e.preventDefault();
+                y--;
+            }
             break;
         case 39:
+            if(limitsBall.right < limitsStage.right) {
+                e.preventDefault();
+                x++
+            };
             break;
         case 40:
+            if(limitsBall.bottom < limitsStage.bottom) {
+                e.preventDefault();
+                y++
+            };
             break;
 
         default:
             break;
     }
+    $ball.style.transform = `translate(${x * 10}px, ${y * 10}px)`;
 }
 export function shortcuts(e){
     /*console.log(e.type);
