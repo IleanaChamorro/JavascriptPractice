d171b2a1ee1ba69ceb930aa0012ab84e8fac5dd3
ileanachamorro <ic.chamorro07@gmail.com>
2022-11-09 12:25:48 -0300

    DOM: Ejercicios Prácticos | Cuenta regresiva (countdown)

---
+++ 12.Ejercicios_dom/dom/cuenta_regresiva.js
@@ -0,0 +1,20 @@
+const d = document;
+
+
+export default function countdown(id, limitDate, finalMessage){
+    const $countdown = d.getElementById(id),
+    countdownDate = new Date(limitDate).getTime();
+
+
+    let countdownTempo = setInterval(() => {
+        let now = new Date().getTime(),
+        limitTime = countdownDate - now,
+        days = Math.floor(limitTime / (1000 * 60 * 60 * 24)),
+        hours = (limitTime % (1000 * 60 * 60 * 24) )/ (1000 * 60 * 60),
+        minutes,
+        seconds;
+
+        $countdown.innerHTML = `<h3>Faltan : ${days} días ${hours} horas ${minutes} minutos ${seconds} segundos</h3>`
+        console.log(countdownDate, now, limitTime);
+    }, 1000); 
+}
--- 12.Ejercicios_dom/js/index_dom.js
+++ 12.Ejercicios_dom/js/index_dom.js
@@ -1,12 +1,14 @@
 import hamburgerMenu from "../dom/script.js";
 import { digitalClock, alarm } from "../dom/reloj.js";
 import { moveBall, shortcuts } from "../dom/teclado.js";
+import countdown from "../dom/cuenta_regresiva.js";
 const d = document;
 
 d.addEventListener("DOMContentLoaded", (e) => {
     hamburgerMenu(".panel-btn", ".panel", ".menu a");
     digitalClock("#reloj", "#activar-reloj", "#desactivar-reloj");
     alarm("assets/car-alarm.mp3", "#activar-alarma", "#desactivar-alarma");
+    countdown("countdown", "Nov 13, 2022 03:32:19", "Feliz Cumpleaños 🤓🤓");
 });
 
 d.addEventListener("keydown", (e) => {
