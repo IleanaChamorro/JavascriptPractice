59e6309e0bf8d67599e0b040a9ec52d890b2cddc
ileanachamorro <ic.chamorro07@gmail.com>
2022-11-14 20:06:04 -0300

    DOM: Ejercicios Prácticos | Botón de Scroll

--- 12.Ejercicios_dom/js/index_dom.js
+++ 12.Ejercicios_dom/js/index_dom.js
@@ -2,13 +2,15 @@ import hamburgerMenu from "../dom/script.js";
 import { digitalClock, alarm } from "../dom/reloj.js";
 import { moveBall, shortcuts } from "../dom/teclado.js";
 import countdown from "../dom/cuenta_regresiva.js";
+import scrollTopButton from "../dom/boton_scroll.js";
 const d = document;
 
 d.addEventListener("DOMContentLoaded", (e) => {
     hamburgerMenu(".panel-btn", ".panel", ".menu a");
     digitalClock("#reloj", "#activar-reloj", "#desactivar-reloj");
     alarm("assets/car-alarm.mp3", "#activar-alarma", "#desactivar-alarma");
-    countdown("countdown", "Nov 13, 2022 17:55:00", "Feliz Cumpleaños 🤓🤓");
+    countdown("countdown", "Nov 13, 2023 00:00:00", "Feliz Cumpleaños 🤓🤓");
+    scrollTopButton(".scroll-top-btn");
 });
 
 d.addEventListener("keydown", (e) => {
