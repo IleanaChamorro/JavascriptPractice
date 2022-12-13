96ea59ca3cd4638a91aa34375c1ccc199cea38c0
ileanachamorro <ic.chamorro07@gmail.com>
2022-12-12 23:58:13 -0300

    Ajax: API Fetch

--- 13.AJAX/js/ajax.js
+++ 13.AJAX/js/ajax.js
@@ -1,4 +1,4 @@
-(() => { 
+/*(() => { 
     //objeto ajax
     const xhr = new XMLHttpRequest(),
     //referencia nodo del dom
@@ -40,4 +40,42 @@
     //método send, envía la solicitud al servidor
     //parámetros: especificar el cuerpo de la solicitud, esto se usa principalmente para solicitudes como PUT. Si el método de solicitud es GETo HEAD, el body parámetro se ignora y el cuerpo de la solicitud se establece en null.
     xhr.send();
+})();
+*/
+
+//Function auto ejecutable
+//Api fetch
+(() => {
+    const $fetch = document.getElementById("fetch"),
+    $fragment = document.createDocumentFragment();
+
+    //then() ejecuta la parte positiva si la promesa se resuelve
+    //then() retorna la respuesta en objeto que no podemos manipular textualmente
+    //catch() parte negativa, captura el error
+
+    fetch("https://jsonplaceholder.typicode.com/user")./*then(res => {
+        console.log(res);
+        //Dependiendo la respuesta podemos convertirla a json o text
+        //res.text(), res.json()
+
+        //validación
+        return res.ok ? res.json() : Promise.reject(res);
+    })*/
+    then(() => (res.ok ? res.json() : Promise.reject((res)))
+    .then((json) => {
+        console.log(json);
+        //$fetch.innerHTML = json;
+    })
+    .catch((err) => {
+        console.log((err) => {
+            console.log(err);
+            let message = err.statusText || "Ocurrió un error";
+            $fetch.innerHTML = `Error ${err.status}: ${message}`;
+        })
+    })
+    .finally(() => 
+        console.log(
+            "Esto se ejecutará independientemente del resultado de la Promesa Fetch"
+        )
+    )
 })();
