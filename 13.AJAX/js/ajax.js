25e777daade12404ee41ce44ee6e16f772f8c720
ileanachamorro <ic.chamorro07@gmail.com>
2022-12-16 23:59:00 -0300

    Libreria Axios

--- 13.AJAX/js/ajax.js
+++ 13.AJAX/js/ajax.js
@@ -85,3 +85,37 @@
         )
     )
 })();
+
+/* API Fetch + Async-Await */
+(() => {
+    const $fetchAsync = document.getElementById("fetch-async"),
+    $fragment = document.createDocumentFragment();
+
+    async function getData(){
+        try{
+            let res = await fetch("https://jsonplaceholder.typicode.com/users"),
+            //espera respuesta para pasarla a json
+            json = res.json;
+            ;
+
+            console.log(res, json);
+            if(!res.ok) throw {status: res.status, statusText: res.statusText}
+
+            json.forEach((el) => {
+                const $li = document.createElement("li");
+                $li.innerHTML = `${el.name} -- ${el.email} -- ${el.phone}`;
+                $fragment.appendChild($li);
+            });
+
+            $fetchAsync.appendChild($fragment);
+        }catch(err){
+            console.log("Estoy en el catch", err);
+            let message = err.statusText || "Ocurrio un error";
+            $fetchAsync.innerHTML = `Error ${err.status}: ${message}`
+        }finally{
+            console.log("Esto se ejecutará independientemente del try...catch")
+        }
+        
+    }
+    getData();
+})
